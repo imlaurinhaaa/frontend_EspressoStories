e307f5b0e132d1b05006a78cebb6b8d077794aad
Bruna Savelli <bruna.savelli@aluno.senai.br>
2025-11-25 16:24:45 -0300

    feat: try to start Sobremesas page

--- src/components/productCard/productCard.jsx
+++ src/components/productCard/productCard.jsx
@@ -1,12 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './productCard.module.css';
 import React from 'react';
+import Link from 'next/link';
 
-export default function ProductCard({ img, alt, name }) {
+export default function ProductCard({ product, onClick }) {
     return (
-        <div className={styles.card}>
-            <Image src={img} alt={alt} width={250} height={200} className={styles.image} />
-            <h2 className={styles.name}>{name}</h2>
+        <div className={styles.productCard} onClick={onClick}>
+            <Image
+                src={product.photo}
+                alt={product.name}
+                width={150}
+                height={150}
+                className={styles.productImage}
+            />
+            <h3 className={styles.productTitle}>{product.name}</h3>
+            <p>{product.description}</p>
+            <p>{product.price}</p>
         </div>
     )
 }
