c07a87eeb3ff3ad8c441294ccb1a576e2f9fdad1
Laura Violla <lauraviolla.tds1.senai@gmail.com>
2025-11-25 11:55:22 -0300

    feat: add products

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,16 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
